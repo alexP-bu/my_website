896d7daa7b54f77da564739317baa253e2005e70
alexP-bu <apozin@bu.edu>
2021-12-20 20:24:29 -0500

    setting up styles

---
+++ php/src/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset ="UTF-8">
+        <title>Alexander Pozin</title>
+        <meta name="description" content="Just a guy that likes to code.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:locale" content="en_US">
+        <meta property="og:type" content="website">
+        <meta property="og:description" content="Just a guy that likes to code.">
+        <meta property="site_name" content="Alexander Pozin">
+        <link rel="stylesheet" href="./css/styles.css"
+    </head>
+
+    <body>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            <ul>    
+        </nav>
+    </body>
+
