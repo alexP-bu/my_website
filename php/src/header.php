c2c82c42c1045e8c94e667a429887d31f2d09dae
alexP-bu <apozin@bu.edu>
2021-12-20 22:05:25 -0500

    working on index page

--- php/src/header.php
+++ php/src/header.php
@@ -9,9 +9,15 @@
         <meta property="og:type" content="website">
         <meta property="og:description" content="Just a guy that likes to code.">
         <meta property="site_name" content="Alexander Pozin">
-        <link rel="stylesheet" href="./css/styles.css"
+        <link rel="stylesheet" href="./css/styles.css">
+        <!--google fonts-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600&display=swap" rel="stylesheet">
     </head>
 
+    
+    <div class="page-container">
     <body>
         <nav>
             <ul>
--- php/src/index.php
+++ php/src/index.php
@@ -1,7 +1,15 @@
 <?php 
 include_once("header.php");
 ?>
-
+<h1>
+    Hello, World!
+</h1>
+<h2>
+    My name is Alex.
+</h2>
+<h2>
+    What's your name?
+<h2>
 <?php
 include_once("footer.php");
 ?>
