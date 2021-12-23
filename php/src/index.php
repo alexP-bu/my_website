f778ae51c9c36ef84c9d159ed7e753f3706c9be4
alexP-bu <apozin@bu.edu>
2021-12-23 18:23:17 -0500

    finished index page

--- php/src/index.php
+++ php/src/index.php
@@ -2,15 +2,12 @@
 include_once("header.php");
 ?>
 <div class="content">
-<h1 class="typed1">
-    Hello, World!
-</h1>
-<h2 class="typed2">
-    My name is Alex. I'm a developer!
-</h2>
-<h3 class="typed3">
-    What would you like to see? 
-</h3>
+    <h1 class="typed1">Hello, World!</h1>
+    <h2 class="typed2">My name is Alex. I'm a developer!</h2>
+    <h3 class="typed3">What would you like to see? </h3>
+    <div class="arrow-wrapper">
+        <i class="fas fa-arrow-down fa-3x"></i>
+    </div>
 </div>
 <?php
 include_once("footer.php");
