31e465b5a21030ad1a055cb7db619db8d56b1f29
alexP-bu <apozin@bu.edu>
2021-12-30 20:51:59 -0500

    refactored javascript

--- php/src/js/scripts.js
+++ php/src/js/scripts.js
@@ -1,18 +1,13 @@
 $(document).ready(function (){
     var buttons = $('.navbar-item');
-    var home = document.getElementsByClassName('home-content');
-    var work = document.getElementsByClassName('work-content');
-    var about = document.getElementsByClassName('about-content');
-    var contact = document.getElementsByClassName('contact-content');
-
+    //hide unloaded content
     $(work).hide();
     $(about).hide();
     $(contact).hide();
-
+    //set home to loaded
     var currentLoaded = home;
-
     document.getElementById('home').classList.add('removed');
-
+    //function to change pages on click
     buttons.on('click', function (evt) {
         //animation for navbar
         $(buttons).each(function(index){
@@ -22,49 +17,16 @@ $(document).ready(function (){
         //on click, unload current content from page
         unloadContent();
         //load new content
-        switch(this.id){
-            case "home":
-                console.log("home!");
-                loadHomeContent();
-                break;
-            case "work":
-                console.log("work!");
-                loadWorkContent();
-                break;
-            case "about":
-                console.log("about!");
-                loadAboutContent();
-                break;
-            case "contact":
-                console.log("contact!");
-                loadContactContent();
-                break;    
-        }
+        loadContent(document.getElementsByClassName(this.id + '-content'));
     });
-    
+    //unload webpage content
     function unloadContent(){
         $(currentLoaded).fadeOut();
         $(currentLoaded).hide();
-        console.log("unloaded!");
     }
-
-    function loadHomeContent(){
-        $(home).fadeIn();
-        currentLoaded = home;
-    }
-
-    function loadWorkContent(){
-        $(work).fadeIn();
-        currentLoaded = work;
-    }
-
-    function loadAboutContent(){
-        $(about).fadeIn();
-        currentLoaded = about;
-    }
-
-    function loadContactContent(){
-        $(contact).fadeIn();
-        currentLoaded = contact;
+    //load page
+    function loadContent(item){
+        $(item).fadeIn();
+        currentLoaded = item;
     }
 });
